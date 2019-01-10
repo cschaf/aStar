b056378a5468d4bcafeb8e6cbde68e458813f971
Christian Schaf <cschaf@outlook.com>
2019-01-10 21:28:38 +0100

    [Add] drawing path of a-star seeker

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,39 +8,93 @@ public class Unit : MonoBehaviour {
 
 	public Transform target;
 	public float speed;
-    public Text winnerText;
+  public Text winnerText;
+  private LineRenderer lineRenderer;
+  private List<GameObject> connectors;
 	Vector3[] path;
 	int targetIndex;
 	
 	void Start(){
-		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-	}
+    connectors = new List<GameObject>();
+    // Get the line renderer component
+    lineRenderer = gameObject.GetComponent<LineRenderer>();
+    lineRenderer.startColor = Color.black;
+  }
 	
 	void Update(){
 		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    UpdateSphereConnectors();
 	}
-	
-	public void OnPathFound(Vector3[] newPath, bool pathSuccessful){
+
+  IEnumerator DrawUnitPath() {
+    if(path != null){
+      List<GameObject> spheres = new List<GameObject>();
+      if(path.Length == 1) {
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, path[0]);
+        lineRenderer.SetPosition(1, transform.position);
+      }
+      else if (path.Length > 1) {
+        for(int i = targetIndex; i < path.Length; i++) {
+          // Draw connector sphere
+          DrawSphere(path[i]);
+          if(i == targetIndex) {
+            lineRenderer.SetPosition(i, path[i]);
+            lineRenderer.SetPosition(i, transform.position);
+          }
+          else {
+            lineRenderer.SetPosition(i, path[i - 1]);
+            lineRenderer.SetPosition(i, path[i]);
+          }
+        }
+      }
+    }
+    yield return null;
+
+  }
+
+  private void DrawSphere(Vector3 pos) {
+    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    sphere.transform.localScale = new Vector3(5, 5, 5);
+    sphere.transform.position = pos;
+    sphere.GetComponent<Renderer>().material.color = Color.black;
+    connectors.Add(sphere);
+  }
+
+  private void UpdateSphereConnectors() {
+    for(int i = 0; i < connectors.Count; i++) {
+      if(connectors[i].transform.position.z < gameObject.transform.position.z) {
+        if(connectors[i] != null || !ReferenceEquals(connectors[i], null)) {
+          GameObject current = connectors[i];
+          connectors.Remove(connectors[i]);
+          Destroy(current);
+        }
+      }
+    }
+  }
+
+  public void OnPathFound(Vector3[] newPath, bool pathSuccessful){
 		if(pathSuccessful){
-            path = newPath;
-			StopCoroutine("FollowPath");
-			StartCoroutine("FollowPath");
+        path = newPath;
+        lineRenderer.positionCount = path.Length;
+			  StopCoroutine("FollowPath");
+		  	StartCoroutine("FollowPath");
 		}
 	}
 	
 	public void OnDrawGizmos(){
-		if(path != null){
-			for(int i=targetIndex; i<path.Length; i++){
-				Gizmos.color = Color.black;
-				Gizmos.DrawCube(path[i], Vector3.one);
-				if(i == targetIndex){
-					Gizmos.DrawLine(transform.position, path[i]);
-				}
-				else{
-					Gizmos.DrawLine(path[i-1], path[i]);
-				}
-			}
-		}
+		//if(path != null){
+		//	for(int i=targetIndex; i<path.Length; i++){
+		//		Gizmos.color = Color.black;
+		//		Gizmos.DrawCube(path[i], Vector3.one);
+		//		if(i == targetIndex){
+		//			Gizmos.DrawLine(transform.position, path[i]);
+		//		}
+		//		else{
+		//			Gizmos.DrawLine(path[i-1], path[i]);
+		//		}
+		//	}
+		//}
 	}
 	
 	IEnumerator FollowPath(){
@@ -49,7 +103,9 @@ public class Unit : MonoBehaviour {
 		   } else {
 			   if (path.Length > 0)
 			   {
-				   Vector3 currentWaypoint = path[0];
+          StopCoroutine("DrawUnitPath");
+          StartCoroutine("DrawUnitPath");
+          Vector3 currentWaypoint = path[0];
 
 				   while (true)
 				   {
